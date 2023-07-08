10080fec84b26644cff8bad8a4bc8a4bfc2471c7
ronewa mavhungu <ronewagilbert3@gmail.com>
2023-07-08 19:46:22 +0200

    adding routes

--- apps/cryptocurrency/src/app/app.routes.ts
+++ apps/cryptocurrency/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { 
+    path: '', component: NxWelcomeComponent, pathMatch: 'full'
+  },
+  {
+    path: 'frontend', loadChildren : ()=> import('@crypocurrency/frontend-ui').then(m => m.FrontendUiModule)
+  },
+  {
+    path: 'product', loadChildren: ()=> import('@crypocurrency/pruducts').then(m=>m.PruductsModule)
+  }
+];
