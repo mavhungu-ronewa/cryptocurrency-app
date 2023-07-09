cd37c3eee729a460a712c2ad65af61d21181c957
ronewa mavhungu <ronewamavhungu.rm@gmail.com>
2023-07-09 02:17:25 +0200

    linking header

--- apps/cryptocurrency/src/app/app.component.ts
+++ apps/cryptocurrency/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HeaderComponent],
   selector: 'crypocurrency-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
